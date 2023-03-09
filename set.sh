7dbabe2e88404ba58088eeb50867913cfee5e720
itc-n22003 <104553685+itc-n22003@users.noreply.github.com>
2023-03-09 10:21:18 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -6,7 +6,7 @@ sed -i "s/PASSWORD/$PASS/g" ./*
 ./brave.sh
 ./install.sh
 echo $PASS | sudo -S crontab cron.edit
-sudo mv auto sd vpn /usr/local/bin/
+sudo mv ./command/* /usr/local/bin/
 
 echo "complete"
 sleep 2
