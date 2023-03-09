1a61459a5557350980e40ce23972f850e087822f
itc-n22003 <104553685+itc-n22003@users.noreply.github.com>
2023-03-09 18:00:44 +0900

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -3,6 +3,6 @@
 PASS="PASSWORD"
 echo $PASS | sudo -S apt install curl
 echo $PASS | sudo -S curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-echo $PASS | sudo -S apt update
-echo $PASS | sudo -S apt install brave-browser
+echo $PASS | (echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list)
+echo $PASS | sudo -S apt -y update
+echo $PASS | sudo -S apt -y tall brave-browser
